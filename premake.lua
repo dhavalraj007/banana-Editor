6e70f65d56ceac14c34a03490d0d4911dd6947be
Dhaval Raj <rdjcoding@gmail.com>
2021-09-19 12:21:06 +0530

    Added premake.exe and premake.lua file

---
+++ premake.lua
@@ -0,0 +1,26 @@
+workspace "banana"
+    startproject "bananaEditor"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+project "bananaEditor"
+    location "bananaEditor"
+    kind "ConsoleApp"
+    
+    language "C++"
+    cppdialect "C++17"
+    flags
+    {
+        "FatalWarnings"
+    }
+
+    files
+    {
+        "%{prj.name}/src/**.h"
+        "%{prj.name}/src/**.cpp"
+    }
