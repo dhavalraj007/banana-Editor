96b261a91aa1f486e5c6d5f5c6c37c80a8e121ad
Dhaval Raj <rdjcoding@gmail.com>
2021-09-20 13:24:25 +0530

    fixed bug of not detecting platform as windows

--- tools/globals.py
+++ tools/globals.py
@@ -1,15 +1,17 @@
 V_MAJOR = 1
 V_MINOR = 0
-
+ENGINE_NAME = "banana"
+PROJECT_NAME = "bananaEditor"
 import sys,platform
 
 PLATFORM = sys.platform
 for x in platform.uname():
     if "microsoft" in x.lower():
-        PLATFORM = "windows"
+        PLATFORM = "win32"
 
 def IsWindows():
-    return PLATFORM == "windows"
+    print(PLATFORM)
+    return PLATFORM == "win32"
 def IsLinux():
     return PLATFORM == "linux"
 def IsMac():
