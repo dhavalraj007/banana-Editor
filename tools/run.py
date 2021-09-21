5c741b1fca6ed72d2e2301ce0ecf1b3fb316e2cb
Dhaval Raj <rdjcoding@gmail.com>
2021-09-21 12:27:50 +0530

    edited for linux

--- tools/run.py
+++ tools/run.py
@@ -1,8 +1,8 @@
 import subprocess,globals
 
 CONFIG = "Debug"
-exePath = ".\\bin\\{}\\{}".format(CONFIG,globals.PROJECT_NAME)
+exePath = "./bin/{}/{}".format(CONFIG,globals.PROJECT_NAME)
 if globals.IsWindows():
     subprocess.call(["cmd.exe","/c","cd {} && {}.exe".format(exePath,globals.PROJECT_NAME)])
 else:
-    subprocess.call(["cd {} && {}".format(exePath,globals.PROJECT_NAME)])
+    subprocess.call(["{}{}".format(exePath,globals.PROJECT_NAME)],cwd="exepath")
